d4b8d753b746453ef62d5cbdbbc666839076820e
Adam Priest <adampriest@pop-os.localdomain>
2020-11-08 20:35:06 -0500

    created resources for moons which is a join table between user/planets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :moons
   #root
   root "sessions#home"
 
---
+++ db/migrate/20201109013441_create_moons.rb
@@ -0,0 +1,14 @@
+class CreateMoons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :moons do |t|
+      t.string :name
+      t.string :planet_moon_orbits
+      t.text :description
+      t.string :length_of_year
+      t.references :user, null: false, foreign_key: true
+      t.references :planet, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
