e8a76ea9b83db2e62ec20a8f0ca4f164313dc04c
Adam Priest <adampriest@pop-os.localdomain>
2020-11-18 20:50:55 -0500

    added successful login with OmniAuthentication using Google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,29 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(username: params[:user][:username])
-        if user && user.authenticate(params[:user][:password])
-            session[:user_id] = user.id
-            redirect_to user_path(user)
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
         else
             flash[:message] = "Incorrect login info. Please try again."
             redirect_to '/login'
         end 
     end
 
-    def google_login
-        byebug
-    end
-
     def destroy
         session.clear
         redirect_to root_path
+    end
+
+    def google_login
+        user_email = request.env['omniauth.auth']['info']['email']
+        user_name = request.env['omniauth.auth']['info']['name']
+        @user = User.find_or_create_by(email: user_email) do |user|
+            user.username = user_name
+            user.password = SecureRandom.hex
+        end
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
     end
 end
